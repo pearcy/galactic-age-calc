868696269f95f7e5eb05b09a272e157e6e01e3b8
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-26 13:10:38 -0700

    test succeeded - correctly create a date object with a month, day, year attribute

--- src/galactic.js
+++ src/galactic.js
@@ -2,11 +2,11 @@
 
 export class AgeInput {
   constructor(month, day, year, planet, life) {
-  // this.month = month;
-  // this.day = day;
-  // this.year = year;
-  // this.planet = planet;
-  // this.life = life;
+  this.month = month;
+  this.day = day;
+  this.year = year;
+  this.planet = planet;
+  this.life = life;
   }
 
 

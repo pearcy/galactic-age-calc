41a4be515b1da202df36936479953487d28d31dd
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-26 13:09:33 -0700

    meaninful fail - test with date input

--- _tests_/gal.test.js
+++ _tests_/gal.test.js
@@ -1,1 +1,11 @@
-// import { AgeInput } from "./../src/galactic.js";
+import { AgeInput } from "./../src/galactic.js";
+
+describe ('AgeInput', () => {
+  test('input should correctly create a date object with a month, day, year attribute', () => {
+    let date = new AgeInput(11, 3, 1980);
+    expect(date.month).toEqual(11);
+    expect(date.day).toEqual(3);
+    expect(date.year).toEqual(1980);
+  });
+});
+
--- src/galactic.js
+++ src/galactic.js
@@ -2,27 +2,29 @@
 
 export class AgeInput {
   constructor(month, day, year, planet, life) {
-  this.month = month;
-  this.day = day;
-  this.year = year;
-  this.planet = planet;
-  this.life = life;
+  // this.month = month;
+  // this.day = day;
+  // this.year = year;
+  // this.planet = planet;
+  // this.life = life;
   }
 
 
-  calculateAge() {
-    let m = String(this.month);
-    let d = String(this.day);
-    let y = String(this.year);
-    let dob = m + '-' + d + '-' + y; 
-    let birthday = new Date(dob);
+  // calculateAge() {
+  //   let m = String(this.month);
+  //   let d = String(this.day);
+  //   let y = String(this.year);
+  //   let dob = m + '-' + d + '-' + y; 
+  //   let birthday = new Date(dob);
   
-    let diff = Date.now() - birthday.getTime();
-    let getAge = new Date(diff); 
+  //   let diff = Date.now() - birthday.getTime();
+  //   let getAge = new Date(diff); 
 
-    let lifeExpectancy = this.planet * this.life;
-    console.log(lifeExpectancy);
-    // let calc = Math.abs(getAge.getUTCFullYear() - 1970);
-    let calc = Math.abs(getAge.getUTCFullYear() - 1970) * this.planet;
+  //   let lifeExpectancy = this.planet * this.life;
+  //   console.log(lifeExpectancy);
+  //   // let calc = Math.abs(getAge.getUTCFullYear() - 1970);
+  //   let calc = Math.abs(getAge.getUTCFullYear() - 1970) * this.planet;
   
-  
+  // }
+
+}
